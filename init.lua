4c524dc1bfb4eb9612c6b4eb27651f28b5350f3a
Carlos Henrique <ch.henriquevinha@gmail.com>
2025-03-29 20:42:31 -0300

    Fix runtimepath

--- init.lua
+++ init.lua
@@ -1,5 +1,7 @@
 -- Main configuration Neovim
 -- Autor: Carlos
+package.path = package.path .. ';' .. vim.fn.stdpath('config') .. '/?.lua'
+
 require('core.options')
 require('core.keymaps')
 require('core.lsp').setup()
