5f2bc88e9016bc86ecd7bfcf5e2015c0211428a9
Carlos Henrique <ch.henriquevinha@gmail.com>
2025-03-29 21:55:46 -0300

    Enable code folding treesitter

--- lua/core/treesitter.lua
+++ lua/core/treesitter.lua
@@ -1,5 +1,14 @@
+vim.o.foldmethod = 'expr'
+vim.o.foldexpr = 'nvim_treesitter#foldexpr()'
+vim.o.foldlevelstart = 99
+
 vim.api.nvim_create_autocmd("FileType", {
-    callback = function()
-        pcall(vim.treesitter.start)
-    end
+	callback = function()
+		pcall(vim.treesitter.start)
+	end
 })
+--    zc (fold)
+--    zo (unfold)
+--    zM (fold all)
+--    zR (unfold all)
+--    za – Toggles between folding and unfolding the code block at the current line.
