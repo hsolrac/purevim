babe13d65fb696a60859f172ab6ae56cb2ce59ad
Rahul Martim Juliato <rahul.juliato@gmail.com>
2025-09-06 21:35:18 -0300

    feat: add search require + formatting

--- init.lua
+++ init.lua
@@ -1,9 +1,10 @@
 -- Main configuration Neovim
 -- Autor: Carlos
-package.path = package.path .. ';' .. vim.fn.stdpath('config') .. '/?.lua'
+package.path = package.path .. ";" .. vim.fn.stdpath("config") .. "/?.lua"
 
-require('core.options')
-require('core.keymaps')
-require('core.lsp').setup()
+require("core.options")
+require("core.keymaps")
+require("core.lsp").setup()
 require("core.autocmd")
 require("core.treesitter")
+require("core.search")
